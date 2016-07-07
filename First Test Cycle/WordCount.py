1303c0da835d87762304b1f1f72924151a8d4556
Steven Mayner <steven.h.mayner@gmail.com>
2016-07-07 16:24:18 -0400

    7/7 Commit

--- First Test Cycle/WordCount.py
+++ First Test Cycle/WordCount.py
@@ -1,4 +1,4 @@
-# Beginning of file, imports go up here.
+# Beginning of file for my sake, imports go up here.
 import string
 
 # Function Definitions
@@ -16,52 +16,54 @@ def safeInput(promptText):
         except FileNotFoundError:
             print("File not found, please ensure the text document(s) are in the same folder as the WordCount program.")
         except ValueError:
-            print("File in incorrect format, please make sure the essay is a .txt file and that you add .txt to the name of the file.")
+            print("File in incorrect format, please make sure the essay is a .txt file.")
 
 def safeDivision(dividend, divisor):
     if divisor == 0:
         divisor += 1
-        print ("Division by zero occurred; answer may be unreliable.")
+    print ("Division by zero occurred; answer may be unreliable.")
     return dividend / divisor
 
 def fileWordCount(filename):
-    with open(filename, 'r') as rawFile:
-        rawString = rawFile.read()
+    rawFile = open(filename)
+    rawString = rawFile.read()
+    rawFile.close()
     sentenceCount = 0
     wordCount = 0
-    punctuation = [".","?",",",":",";","!"] # what counts as punctuation
+    punctuation = [".","?",",",":",";"] # what counts as punctuation
     minWordLength = 4 # the minimum length of a word to count; declared as a variable for consistency
     splitString = str.split(rawString)
     for word in splitString:
         if word[-1] in punctuation:
-            sentenceCount += 1 # word ends in punctuation, so we count a sentence
+            sentenceCount += 1
             if len(word)-1 >= minWordLength: # -1 to not count the punctuation
-                wordCount += 1 # word is long enough, so we count it
+                wordCount += 1
             elif word[-1] == "." and word[0] in string.ascii_uppercase: # the word is less than minimum length (else) AND ends in a . AND begins with a capital letter
                 sentenceCount -= 1 # the "sentence" is actually an abbreviation, so take the sentence out again
         elif len(word) >= minWordLength:
             wordCount += 1 # word does not end in punctuation and is long enough, so we count it
-    return safeDivision(wordCount,sentenceCount)
+    return wordCount / sentenceCount
 
 def singleFile(filename):
     result = fileWordCount(filename)
-    with open("WordCount.txt",'a') as outputFile:
-        outputFile.write("The average words per sentence for {0} was {1}\n".format(filename,result))
     print("The average words per sentence of that file was:",result)
-    return result # this return does nothing for single files, but allows integration with batchFile
 
 def batchFile():
+    outputFile = open("WordCount.txt",'w')
+    outputFile.write("")
     finalSum = 0
     fileCount = 0
-    currentInput = safeInput("Enter the name of the next text file you want to scan, or 'done' if you are done: ") 
+    currentInput = safeInput("Enter the name of the next text file you want to scan, or 'done' if you are done: ")
+    outputFile = open("WordCount.txt",'a')   
     while currentInput != "done":
-        currentAverage = singleFile(currentInput)
+        currentAverage = fileWordCount(currentInput)
         finalSum += currentAverage
         fileCount += 1
+        outputFile.write("The average words per sentence for {0} was {1}.\n".format(currentInput,currentAverage))
         currentInput = safeInput("Enter the name of the next text file you want to scan, or 'done' if you are done: ")
-    finalResult = safeDivision(finalSum,fileCount)
-    with open("WordCount.txt",'a') as outputFile:
-        outputFile.write("The average words per sentence for those files was {0}".format(finalResult))
+    finalResult = finalSum / fileCount
+    outputFile.write("The average words per sentence for those files was {0}.".format(finalResult))
+    outputFile.close()
     print ("The average words per sentence of those files was:",finalResult)
     print ("You can also see specific details in WordCount.txt")
 
@@ -70,14 +72,11 @@ def main():
     print("Hello, welcome to WordCount!")
     print("Please ensure the text document(s) you want to read are in the same folder as the WordCount program.")
     firstInput = safeInput("Then enter the name of the text file you want to scan, or 'batch' if you want to scan multiple: ")
-    outputFile = open("WordCount.txt",'w').close() # wipes the output file clean because singleFile appends
-    if firstInput == "done":
-        print("No files were scanned. Please close and restart WordCount to try again.")    
-    elif firstInput != "batch":
+    if firstInput != "batch":
         singleFile(firstInput)
     else:
         batchFile()
-    input("Press enter to finish and close the program.")
+    print("Yay words!")
 
 # End of File, only main() after this line
 main()
---
+++ README.txt
@@ -0,0 +1,11 @@
+Hello and welcome to WordCount!
+
+This readme document will walk you through using your WordCount program to best effect.
+
+First and foremost, Python 3 or later must be installed on your system. You can find a download link https://www.python.org/downloads/ here. A default installation will suffice to use WordCount; make sure you download the right version of Python for your system.
+
+In order to utilize WordCount, your essay must be a .txt document (that is, the same kind of file as this readme!) and in the same folder as your WordCount.py file. Do this by dragging the essay file into the place as you downloaded WordCount.py
+
+Once you've done so, double click on the WordCount.py icon, and a window should open with a "Hello, welcome to WordCount!" prompt. Follow the instructions the program gives you.
+
+While the program does display text output for specific users, it also outputs to a file called WordCount.txt, which will be created in the same folder as your WordCount.py program. Open that to see your results. Please note that this file will be overwritten every time WordCount is run! If you want to save a particular result, either rename 
